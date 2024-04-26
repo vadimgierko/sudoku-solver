ca672bde91f7bda3ac703b72dee3a45199ef906d
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2024-04-27 00:26:39 +0200

    migrate to next.js

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
 			<h1>Sudoku Solver</h1>
 			<p>
 				<strong>
-					This app solves an easy sudoku. At the moment I'm working on more
+					This app solves an easy sudoku. At the moment I&apos;m working on more
 					advanced algorithm, which will be able to solve more comlicated
 					sudoku. When it will be ready, it will be merged with the existing
 					stable version of the app & the message will be removed.
